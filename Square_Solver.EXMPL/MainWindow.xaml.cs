6041f17f1e3e0cd415b893ad37f3ea645b81e08e
j1sk1ss <fotikolay222@gmail.com>
2022-10-24 20:52:36 +0500

    Backend complete

--- Square_Solver.EXMPL/MainWindow.xaml.cs
+++ Square_Solver.EXMPL/MainWindow.xaml.cs
@@ -1,28 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Square_Solver.EXMPL.SCRIPTS;
 
-namespace Square_Solver.EXMPL
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace Square_Solver.EXMPL {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+        }
+        private void Calculate(object sender, RoutedEventArgs e) {
+            AnswerSection.Visibility = Visibility.Visible;
+            AnswerLabelOne.Content = "РЕШЕНИЕ:";
+            try {
+                SquareSolver solver = new(double.Parse( $"{VariableA.Text}"), double.Parse( $"{BCof.Text}{VariableB.Text}"),
+                    double.Parse($"{CCof.Text}{VariableC.Text}"));
+                solver.Calculate();
+                FirstUpperAnswer.Content = -solver.B - solver.D;
+                FirstDownerAnswer.Content = solver.A * 2 + " ";
+                SecondUpperAnswer.Content = -solver.B + solver.D;
+                SecondDownerAnswer.Content = solver.A * 2 + " ";
+                if (solver.SecondValue != 0)
+                {
+                    Answer1.Content = solver.FirstValue;
+                    Answer2.Content = solver.SecondValue;
+                }
+                else
+                {
+                    Answer1.Content = solver.FirstValue;
+                    SecondExample.Visibility = Visibility.Hidden;
+                    SecondValue.Visibility = Visibility.Hidden;
+                }
+            }
+            catch (Exception exception) {
+                MessageBox.Show($"{e}", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
         }
     }
 }
---
+++ Square_Solver.EXMPL/SCRIPTS/SQUARE_SOLVER.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace Square_Solver.EXMPL.SCRIPTS
+{
+    public class SquareSolver
+    {
+        public double D { get; set; }
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public double FirstValue { get; set; }
+        public double SecondValue { get; set; }
+        public SquareSolver() {
+            A = 0;
+            B = 0;
+            C = 0;
+        }
+        public SquareSolver(double a, double b, double c) {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public void Calculate() {
+            D = B * B - 4 * A * C;
+            switch (D) {
+                case 0:
+                    FirstValue = Math.Round((-B - Math.Sqrt(D)) / (2 * A), 5);
+                    MessageBox.Show("Корень только один!", "Предупреждение.", MessageBoxButton.OK);
+                    return;
+                case < 0:
+                    MessageBox.Show("Корней нет!", "Предупреждение.", MessageBoxButton.OK);
+                    return;
+                default:
+                    FirstValue = Math.Round((-B - Math.Sqrt(D)) / (2 * A), 5);
+                    SecondValue = Math.Round((-B + Math.Sqrt(D)) / (2 * A), 5);
+                    break;
+            }
+        }
+    }
+}
